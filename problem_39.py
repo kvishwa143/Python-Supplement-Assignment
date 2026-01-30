992dfee3022f46c7f09113c800be6a0381ff0b3c
kvishwa143 <kvishwa1432k8@gmail.com>
2026-01-30 15:02:55 +0530

    Update problem_39.py

--- problem_39.py
+++ problem_39.py
@@ -3,5 +3,9 @@
 
 dict1 = {"a": 1, "b": 2}
 dict2 = {"c": 3, "d": 4}
-merged = dict1 + dict2
+
+merged = dict1.copy()  # make a copy to avoid changing original
+merged.update(dict2)
+
 print(f"Merged dictionary: {merged}")
+
