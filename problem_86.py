44bb61c8bd2aecbf05928f34720e578b3e57f6ab
kvishwa143 <kvishwa1432k8@gmail.com>
2026-01-30 17:46:46 +0530

    Update problem_86.py

--- problem_86.py
+++ problem_86.py
@@ -6,8 +6,15 @@ def diagonal_sum(matrix):
     total = 0
     for i in range(n):
         total += matrix[i][i]
-        total += matrix[i][n-1-i]
+        total += matrix[i][n - 1 - i]
+    
+    if n % 2 == 1:  
+        total -= matrix[n // 2][n // 2]
+    
     return total
 
-mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+mat = [[1, 2, 3], 
+       [4, 5, 6], 
+       [7, 8, 9]]
+
 print(f"Diagonal sum: {diagonal_sum(mat)}")
